d2912f67f0a3d20c53b8410a4171c79365e933dd
Nick Rathbone <nrathbone@joinhandshake.com>
2025-08-23 21:05:22 -0600

    feat(models): add Review model with relationships to Customer and Item

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from marshmallow import Schema, fields
 
@@ -17,9 +18,11 @@ class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
 
+    # one-to-many: customer --> reviews
+    reviews = relationship("Review", back_populates="customer", cascade="all, delete-orphan")
+
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
-
 
 class Item(db.Model):
     __tablename__ = 'items'
@@ -28,5 +31,25 @@ class Item(db.Model):
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    # one-to-many: item --> reviews
+    reviews = relationship("Review", back_populates="item", cascade="all, delete-orphan")
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+    
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+
+    # foreign keys
+    customer_id = db.Column(db.Integer, ForeignKey("customers.id"))
+    item_id = db.Column(db.Integer, ForeignKey("items.id"))
+
+    # relationships
+    customer = relationship("Customer", back_populates="reviews")
+    item = relationship("Item", back_populates="reviews")
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.comment}>'
